7e20af208807b933125eb54592513700b1a576f6
Shahin Mohammadi <shahin.mohammadi@gmail.com>
2020-11-03 07:16:10 -0500

    Initial commit

---
+++ actionet/install.R
@@ -0,0 +1,5 @@
+install.packages(c('aws.s3', 'optparse'))
+
+devtools::install_github("shmohammadi86/SCINET")
+devtools::install_github("shmohammadi86/NetLibR")
+devtools::install_github("shmohammadi86/ACTIONet", ref = "R-release")
